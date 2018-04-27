0ca972daca645a28724c89fb4e7f6b731a3e368f
fabiokilling <36022541+fabiokilling@users.noreply.github.com>
2018-04-27 17:31:31 +0800

    Update 斗地主分排为3堆比较短的代码.py

--- 斗地主分排为3堆比较短的代码.py
+++ 斗地主分排为3堆比较短的代码.py
@@ -1,18 +1,12 @@
 # -*- coding: utf-8 -*-
-"""
-Created on Fri Apr 27 09:42:10 2018
-
-@author: ArcherX
-"""
-from random import shuffle  
-from random import choice    
+from random import shuffle, choice      
 values = list(range(2, 11)) + 'J Q K A'.split()  
 suits = '菱形 梅花 红桃 黑桃'.split()  
 king = ['大王','小王']
 deck = ['%s%s' % (s, v) for s in suits for v in values]  
 c = king + deck 
 shuffle(c)  
-P = Q = L = []
+L = []
 count=0  
 while c:   
     randomCard = choice(c)  
